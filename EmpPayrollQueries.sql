bfa4b372108ecf89fd1793fab388d33e6892b810
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-03 10:13:11 +0530

    [Madhavee] Retrieve particular data using condition from Table

--- EmpPayrollQueries.sql
+++ EmpPayrollQueries.sql
@@ -14,6 +14,12 @@ insert into employee_payroll
 values ('John',300000,'2008-03-03'),('Alice',500000,'2010-08-10');
 insert into employee_payroll(Emp_name,Emp_salary)
 values ('Harry',450000);
+insert into employee_payroll
+values ('Bill',200000,'2005-07-25'),('Tvisha',800000,'2018-06-18');
 
 ---UC_4 Retrieve all data from table
 select * from employee_payroll;
+
+---UC_5 Retrieve particular data using where condition
+select * from employee_payroll where Emp_name = 'Alice';
+select * from employee_payroll where StartDate between cast('2001-01-01' as date) and GETDATE();
