c3a869d9da3de7081ad8a366217f67e7a1a9a100
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-03 09:51:12 +0530

    [Madhavee] Insert data in Table

--- EmpPayrollQueries.sql
+++ EmpPayrollQueries.sql
@@ -8,3 +8,9 @@ Emp_id int identity(1,1) primary key,
 Emp_name varchar(255),
 Emp_salary float,
 StartDate datetime);
+
+---UC_3 Insert Data In Table
+insert into employee_payroll
+values ('John',300000,'2008-03-03'),('Alice',500000,'2010-08-10');
+insert into employee_payroll(Emp_name,Emp_salary)
+values ('Harry',450000);
