80b2e8a83d68a97416aa5a938d317741ace60275
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-03 09:38:22 +0530

    [Madhavee] Create Table

--- EmpPayrollQueries.sql
+++ EmpPayrollQueries.sql
@@ -1,3 +1,10 @@
 ---UC_1 create a payroll_service database----
 create database payroll_service;
 use payroll_service;
+
+---UC_2 create a employee_payroll table
+create table employee_payroll(
+Emp_id int identity(1,1) primary key,
+Emp_name varchar(255),
+Emp_salary float,
+StartDate datetime);
